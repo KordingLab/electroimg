6db76afc08924685ea815ec41f807ff8e0ffcf82
evadyer <evadevah@gmail.com>
2015-11-12 21:53:17 -0600

    script_runparfor (updated)

--- matlab code/compressive_sensing/script_runparfor.m
+++ matlab code/compressive_sensing/script_runparfor.m
@@ -6,8 +6,8 @@ for i=1:3;
     for j=1:6; 
         parfor k=1:7; 
             F0 = newData((i-1)*15 +1 : min((i-1)*15 +30,60),...
-                (i-1)*15 +1 : min((i-1)*15 +30,60),...
-                (i-1)*15 +1 : min((i-1)*15 +30,60));
+                (i-1)*15 +1 : min((i-1)*15 +30,100),...
+                (i-1)*15 +1 : min((i-1)*15 +30,125),20);
             
             [Frec{i,j,k},whichsamp{i,j,k}] = interpolatefield2(F0,0.2*ii,1e-6);
         end
