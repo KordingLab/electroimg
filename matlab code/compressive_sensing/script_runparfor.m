2f175650febb39117c10c7a5a7c4b03e6b9aa7e2
evadyer <evadevah@gmail.com>
2015-11-13 01:37:15 -0600

    new script

--- matlab code/compressive_sensing/script_runparfor.m
+++ matlab code/compressive_sensing/script_runparfor.m
@@ -1,7 +1,7 @@
 load('NeuroData-11-11(2).mat')
 
 parpool(8)
-for ii=1:4
+for ii=3:4
 for i=1:3; 
     for j=1:6; 
         parfor k=1:7; 
@@ -11,7 +11,7 @@ for i=1:3;
             
             [Frec{i,j,k},whichsamp{i,j,k}] = interpolatefield2(F0,0.2*ii,1e-6);
         end
-        save(['Results-11-12-sampsz-0-pt-',num2str(0.2*ii*10,2)],'Frec','whichsamp')
+        save(['Results-11-13-sampsz-0-pt-',num2str(0.2*ii*10,2)],'Frec','whichsamp')
     end
 end
 end
