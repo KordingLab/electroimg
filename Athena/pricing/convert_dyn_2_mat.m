4fb0ef3704bc3b9e6cf4b40a422462e5abff1f82
Julian Yarkony <julian.e.yarkony@gmail.com>
2017-09-04 21:31:51 -0700

    no message

--- Athena/pricing/convert_dyn_2_mat.m
+++ Athena/pricing/convert_dyn_2_mat.m
@@ -5,10 +5,15 @@ T_new.Theta=[];
 T_new.sub=[];
 T_new.X;
 
+fin_det_val=zeros(G.N,1);
+fin_det_val=zeros(G.N,1);
 for(z=1:numel(Z.red_cost))
-    
     dets_in=G.last_det(Z.Y{z});
     sub_in=Z.Y{z};
     fin_term=G.terminal()
+    
+end
+
+for(d=1:G.N)
     
 end
