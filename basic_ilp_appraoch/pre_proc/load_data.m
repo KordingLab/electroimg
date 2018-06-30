78ae7d37089ea84a9cc2b08b34d86a6789eb00c6
Julian Yarkony <julian.e.yarkony@gmail.com>
2018-06-30 13:24:41 -0700

    no message

--- basic_ilp_appraoch/pre_proc/load_data.m
+++ basic_ilp_appraoch/pre_proc/load_data.m
@@ -10,12 +10,12 @@ F.z=z;
 F.time=time;
 F.bar_E=E;
 
-if(max(sum(E,2))>3.5 || max(sum(E,1))>3.5 )
-    disp('too many kids parents')
-    jy_out_val('max(sum(E,2))',max(sum(E,2)))
-    jy_out_val('max(sum(E,1))',max(sum(E,1)))
-    pause
-end
+%if(max(sum(E,2))>3.5 || max(sum(E,1))>3.5 )
+%    disp('too many kids parents')
+%    jy_out_val('max(sum(E,2))',max(sum(E,2)))
+%    jy_out_val('max(sum(E,1))',max(sum(E,1)))
+%    pause
+%end
 	
 F.W=W;
 
